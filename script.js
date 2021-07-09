cf27b9569c9b369d5944a42346a0eafd9cba7f31
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-07-09 12:07:58 -0300

    Add requirement 2

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function textRender() {
   });
 }
 textRender();
+
+function renderImage() {
+  const imageInput = document.getElementById('meme-insert');
+  imageInput.addEventListener('change', (event) => {
+    const imageContainer = document.getElementById('meme-image');
+    imageContainer.src = URL.createObjectURL(event.target.files[0]);
+  })
+}
+renderImage();
