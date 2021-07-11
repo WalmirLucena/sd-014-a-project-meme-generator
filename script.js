ba1386bea02f3f50b338f36e87bf04d56721727b
Ilan Aragão <ilan_aragao@hotmail.com>
2021-07-11 18:58:30 -0300

    Melhorias gerais e começa bônus 7

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
-textInput.addEventListener('input', function() {
+textInput.addEventListener('input', function () {
   memeText.innerText = textInput.value;
 });
 
 const memeInsert = document.querySelector('#meme-insert');
 const memeImage = document.querySelector('#meme-image');
-memeInsert.addEventListener('change', function(event) {
+memeInsert.addEventListener('change', function (event) {
   memeImage.src = URL.createObjectURL(event.target.files[0]);
-  memeImage.onload = function () {
-    URL.revokeObjectURL(memeImage.src);
-  };
 });
 
 const addBorda = document.querySelector('#meme-image-container');
 const fireButton = document.querySelector('#fire');
-fireButton.addEventListener('click', function() {
+fireButton.addEventListener('click', function () {
   addBorda.style.border = '3px dashed red';
 });
 
 const waterButton = document.querySelector('#water');
-waterButton.addEventListener('click', function() {
+waterButton.addEventListener('click', function () {
   addBorda.style.border = '5px double blue';
 });
 
 const earthButton = document.querySelector('#earth');
-earthButton.addEventListener('click', function() {
+earthButton.addEventListener('click', function () {
   addBorda.style.border = '6px groove green';
 });
