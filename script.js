d603532ec0ffd2b51d891776295434f864ac4033
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-07-11 16:24:53 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const memeText = document.getElementById('meme-text');
 const memeInput = document.getElementById('text-input');
 
-function textoMeme(){
+function textoMeme() {
   memeText.innerText = memeInput.value;
 }
+
+// https://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html
+window.addEventListener('load', function() {
+  document.querySelector('input[type="file"]').addEventListener('change', function() {
+      if (this.files && this.files[0]) {
+          var img = document.querySelector('img');
+          img.onload = () => {
+              URL.revokeObjectURL(img.src);  // no longer needed, free memory
+          }
+
+          img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+      }
+  });
+});
 
 memeInput.addEventListener('keyup', textoMeme);
