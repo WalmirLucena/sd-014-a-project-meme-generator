07164655bbb20742a2ce8b3d42c607f5989e5a16
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-07-11 18:43:11 -0300

    Algumas melhorias

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ const container = document.getElementById('meme-image-container');
 const fireButton = document.getElementById('fire');
 const waterButton = document.getElementById('water');
 const earthButton = document.getElementById('earth');
-const imgMeme1 = document.getElementById('meme-1')
-const imgMeme2 = document.getElementById('meme-2')
-const imgMeme3 = document.getElementById('meme-3')
-const imgMeme4 = document.getElementById('meme-4')
+const blackButton = document.getElementById('black');
+const imgMeme1 = document.getElementById('meme-1');
+const imgMeme2 = document.getElementById('meme-2');
+const imgMeme3 = document.getElementById('meme-3');
+const imgMeme4 = document.getElementById('meme-4');
 
 function textoMeme() {
   memeText.innerText = memeInput.value;
 }
 
 // https://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html
-window.addEventListener('load', function() {
-  document.querySelector('input[type="file"]').addEventListener('change', function() {
+window.addEventListener('load', function abrirJanela() {
+  document.querySelector('input[type="file"]').addEventListener('change', function pegarImagem() {
     if (this.files && this.files[0]) {
       let img = document.querySelector('img');
       img.onload = () => {
@@ -38,42 +39,47 @@ function earth() {
   container.style.border = '6px groove green';
 }
 
+function black() {
+  container.style.border = '1px solid black';
+}
+
 function escMeme1() {
-  let img = document.querySelector('img');
+  const img = document.querySelector('img');
   img.onload = () => {
     URL.revokeObjectURL(img.src);
   };
-  img.src = 'imgs/meme1.png'
+  img.src = 'imgs/meme1.png';
 }
 
 function escMeme2() {
-  let img = document.querySelector('img');
+  const img = document.querySelector('img');
   img.onload = () => {
     URL.revokeObjectURL(img.src);
   };
-  img.src = 'imgs/meme2.png'
+  img.src = 'imgs/meme2.png';
 }
 
 function escMeme3() {
-  let img = document.querySelector('img');
+  const img = document.querySelector('img');
   img.onload = () => {
     URL.revokeObjectURL(img.src);
   };
-  img.src = 'imgs/meme3.png'
+  img.src = 'imgs/meme3.png';
 }
 
 function escMeme4() {
-  let img = document.querySelector('img');
+  const img = document.querySelector('img');
   img.onload = () => {
     URL.revokeObjectURL(img.src);
   };
-  img.src = 'imgs/meme4.png'
+  img.src = 'imgs/meme4.png';
 }
 
 memeInput.addEventListener('keyup', textoMeme);
 fireButton.addEventListener('click', fire);
 waterButton.addEventListener('click', water);
 earthButton.addEventListener('click', earth);
+blackButton.addEventListener('click', black);
 imgMeme1.addEventListener('click', escMeme1);
 imgMeme2.addEventListener('click', escMeme2);
 imgMeme3.addEventListener('click', escMeme3);
