7ec64f3e71bec0c9b1ed4164951f60403db1f739
Ilan Aragão <ilan_aragao@hotmail.com>
2021-07-11 18:37:55 -0300

    Melhorias npm

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
-const memeInsert = document.querySelector('#meme-insert');
-const memeImage = document.querySelector('#meme-image');
-
 textInput.addEventListener('input', function () {
-  memeText.innerHTML = textInput.value;
+  memeText.innerText = textInput.value;
 });
 
+const memeInsert = document.querySelector('#meme-insert');
+const memeImage = document.querySelector('#meme-image');
 memeInsert.addEventListener('change', function (event) {
   memeImage.src = URL.createObjectURL(event.target.files[0]);
   memeImage.onload = function () {
