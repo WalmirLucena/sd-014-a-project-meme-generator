0715bd4ea3a6cbd60edcc6f408fda54897c46ebe
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2021-07-11 20:21:44 -0300

    Fiz lint errors

--- script.js
+++ script.js
@@ -3,6 +3,33 @@ const imageContainer = document.getElementById('meme-image');
 const memeContainer = document.getElementById('meme-image-container');
 const range = document.getElementById('range-width');
 
+function getCurrentPlace(element) {
+  const { top, right, width, height } = element.getBoundingClientRect();
+  return { pageX: right, pageY: top, width, height };
+}
+
+function getMaxRange() {
+  const containerPlace = getCurrentPlace(imageContainer);
+  const { pageX, pageY, width } = getCurrentPlace(textContainer);
+  const maxWidth = containerPlace.pageX - pageX + width;
+  const maxHeight = containerPlace.pageY + containerPlace.height - pageY;
+  return { maxWidth, maxHeight };
+}
+
+function changeTextMax() {
+  const { maxHeight } = getMaxRange();
+  textContainer.style.maxWidth = `${range.value}px`;
+  textContainer.style.maxHeight = `${maxHeight}px`;
+}
+
+function updateRangeMax() {
+  const { maxWidth } = getMaxRange();
+  range.max = maxWidth;
+  range.value = maxWidth;
+  changeTextMax();
+}
+updateRangeMax();
+
 function textRender() {
   const inputText = document.getElementById('text-input');
   inputText.addEventListener('keyup', () => {
@@ -11,15 +38,68 @@ function textRender() {
 }
 textRender();
 
+function getDefaultTextPlace() {
+  const { pageY, pageX, width } = getCurrentPlace(imageContainer);
+  return { pageX: pageX - width, pageY };
+}
+
+function moveText(event) {
+  let { pageX, pageY } = event;
+  pageX += 5;
+  pageY += 5;
+  textContainer.style.left = `calc(${pageX}px - 7%)`; //  CSS main-content
+  textContainer.style.top = `calc(${pageY}px - 50px)`;
+}
+
+function textMover() {
+  imageContainer.addEventListener('mousemove', (event) => {
+    moveText(event);
+    updateRangeMax();
+  });
+  imageContainer.addEventListener('mouseover', (event) => {
+    moveText(event);
+  });
+}
+textMover();
+
+function placeText() {
+  window.scrollTo(0, 0); //  prevent bug in text
+  const defaultTextPlace = getDefaultTextPlace();
+  let currentPlace = defaultTextPlace;
+  moveText(defaultTextPlace);
+  imageContainer.addEventListener('mouseleave', () => {
+    moveText(currentPlace);
+    updateRangeMax();
+  });
+  imageContainer.addEventListener('click', (event) => {
+    const { pageX, pageY } = event;
+    currentPlace = { pageX, pageY };
+  });
+  imageContainer.addEventListener('dblclick', () => {
+    currentPlace = defaultTextPlace;
+    updateRangeMax();
+  });
+}
+placeText();
+
 function renderImage() {
   const imageInput = document.getElementById('meme-insert');
-  imageInput.addEventListener('change', (event) => {  
+  imageInput.addEventListener('change', (event) => {
     imageContainer.src = URL.createObjectURL(event.target.files[0]);
     updateRangeMax();
     placeText();
-  })
+  });
 }
 renderImage();
+
+function removeOldBorder(borders) {
+  const bordersArray = Object.values(borders);
+  for (let i = 0; i < bordersArray.length; i += 1) {
+    if (memeContainer.classList.contains(bordersArray[i])) {
+      memeContainer.classList.remove(bordersArray[i]);
+    }
+  }
+}
 
 function changeBorder() {
   const fire = document.getElementById('fire');
@@ -28,19 +108,11 @@ function changeBorder() {
   const borders = {
     fire: 'fire-border',
     water: 'water-border',
-    earth: 'earth-border'
+    earth: 'earth-border',
   };
-  function removeOldBorder() {
-    const bordersArray = Object.values(borders);
-    for (const border of bordersArray) {
-      if (memeContainer.classList.contains(border)) {
-        memeContainer.classList.remove(border);
-      }
-    }
-  }
   function changeBorderStyle(event) {
     const { className } = event.target;
-    removeOldBorder();
+    removeOldBorder(borders);
     memeContainer.classList.add(borders[className]);
   }
   fire.addEventListener('click', changeBorderStyle);
@@ -55,83 +127,18 @@ function selectPreSelectedMeme() {
     const { target } = event;
     if (!target.classList.contains('meme')) return;
     const style = window.getComputedStyle(target);
-    const image = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+    const image = style.backgroundImage.slice(4, -1).replace(/"/g, '');
     imageContainer.src = image;
     updateRangeMax();
     placeText();
-  })
+  });
 }
 selectPreSelectedMeme();
-
-function getCurrentPlace(element) {
-  const { top, right, width, height } = element.getBoundingClientRect();
-  return {pageX: right, pageY: top, width, height};
-}
-
-function placeText() {
-  window.scrollTo(0, 0); //prevent bug in text
-  function getDefaultTextPlace() {
-    const {pageY, pageX, width} = getCurrentPlace(imageContainer);
-    return {pageX: pageX - width, pageY: pageY};
-  }
-  const defaultTextPlace = getDefaultTextPlace();
-  let currentPlace = defaultTextPlace;
-  function moveText(event) {
-    let { pageX, pageY } = event;
-    pageX += 5;
-    pageY += 5;
-    textContainer.style.left = `calc(${pageX}px - 7%)`; // CSS main-content
-    textContainer.style.top = `calc(${pageY}px - 50px)`; 
-  }
-  moveText(defaultTextPlace);
-  imageContainer.addEventListener('mousemove', (event) => {
-    moveText(event);
-    updateRangeMax();
-  });
-  imageContainer.addEventListener('mouseover', (event) => {
-    moveText(event);
-  });
-  imageContainer.addEventListener('mouseleave', () => {
-    moveText(currentPlace);
-    updateRangeMax();
-  });
-  imageContainer.addEventListener('click', (event) => {
-    const { pageX , pageY} = event;
-    currentPlace = {pageX , pageY};
-  });
-  imageContainer.addEventListener('dblclick', () => {
-    currentPlace = defaultTextPlace;
-    updateRangeMax();
-  });
-}
-placeText();
-
-function getMaxRange() {
-  const containerPlace = getCurrentPlace(imageContainer);
-  const { pageX, pageY, width } = getCurrentPlace(textContainer);
-  const maxWidth = containerPlace.pageX - pageX + width;
-  const maxHeight = containerPlace.pageY + containerPlace.height - pageY;
-  return {maxWidth, maxHeight};
-}
-
-function updateRangeMax() {
-  const { maxWidth } = getMaxRange()
-  range.max = maxWidth;
-  range.value = maxWidth
-  changeTextMax();
-}
-updateRangeMax();
-
-function changeTextMax() {
-  const { maxHeight } = getMaxRange();
-  textContainer.style.maxWidth = range.value + 'px';
-  textContainer.style.maxHeight = maxHeight + 'px';
-}
 
 function textRange() {
   range.addEventListener('mousemove', () => {
     changeTextMax();
-  })
+  });
 }
 textRange();
 
@@ -140,16 +147,16 @@ function setTextRange() {
   const textWeight = document.getElementById('text-weight');
   function setTextSize() {
     const size = textSize.value;
-    textContainer.style.fontSize = size + 'px';
+    textContainer.style.fontSize = `${size}px`;
   }
   setTextSize();
   function setTextWeight() {
     const weight = textWeight.value;
-    textContainer.style.fontWeight = weight
+    textContainer.style.fontWeight = weight;
   }
   setTextWeight();
-  textSize.addEventListener('mousemove' , () => {
-    setTextSize(); 
+  textSize.addEventListener('mousemove', () => {
+    setTextSize();
   });
   textWeight.addEventListener('mousemove', () => {
     setTextWeight();
@@ -169,7 +176,7 @@ function textShadow() {
   shadow();
   checkbox.addEventListener('change', () => {
     shadow();
-  }); 
+  });
 }
 textShadow();
 
@@ -185,22 +192,22 @@ function textColor() {
 }
 textColor();
 
+function setMinWidth(currentWidth) {
+  const style = window.getComputedStyle(imageContainer);
+  const minWidth = style.getPropertyValue('min-width');
+  if (currentWidth < minWidth) {
+    imageContainer.style.minWidth = `${currentWidth}px`;
+    imageContainer.style.minHeight = '1px';
+  } else {
+    imageContainer.style.minWidth = `${minWidth}px`;
+  }
+}
+
 function screenResize() {
   const main = document.querySelector('main');
   function setImageMaxWidth() {
     const { width } = main.getBoundingClientRect();
-    imageContainer.style.maxWidth = width + 'px';
-  }
-  function setMinWidth(currentWidth) {
-    const style = window.getComputedStyle(imageContainer);
-    const minWidth = style.getPropertyValue('min-width');
-    if (currentWidth < minWidth) {
-      imageContainer.style.minWidth = currentWidth + 'px';
-      imageContainer.style.minHeight = '1px'
-      return;
-    }else {
-      imageContainer.style.minWidth = minWidth + 'px';
-    }
+    imageContainer.style.maxWidth = `${width}px`;
   }
   setImageMaxWidth();
   window.addEventListener('resize', () => {
