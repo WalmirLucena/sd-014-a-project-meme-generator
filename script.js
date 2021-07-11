7b7d1e9871bbfcae9639445ff6f1039b6400ac8d
Paulo Renan Almeida <able.paulorenan@gmail.com>
2021-07-11 18:21:50 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const container = document.getElementById('meme-image-container');
 const fireButton = document.getElementById('fire');
 const waterButton = document.getElementById('water');
 const earthButton = document.getElementById('earth');
+const imgMeme1 = document.getElementById('meme-1')
+const imgMeme2 = document.getElementById('meme-2')
+const imgMeme3 = document.getElementById('meme-3')
+const imgMeme4 = document.getElementById('meme-4')
 
 function textoMeme() {
   memeText.innerText = memeInput.value;
@@ -12,14 +16,13 @@ function textoMeme() {
 // https://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html
 window.addEventListener('load', function() {
   document.querySelector('input[type="file"]').addEventListener('change', function() {
-      if (this.files && this.files[0]) {
-          var img = document.querySelector('img');
-          img.onload = () => {
-              URL.revokeObjectURL(img.src);  // no longer needed, free memory
-          }
-
-          img.src = URL.createObjectURL(this.files[0]); // set src to blob url
-      }
+    if (this.files && this.files[0]) {
+      let img = document.querySelector('img');
+      img.onload = () => {
+        URL.revokeObjectURL(img.src);  // no longer needed, free memory
+      };
+      img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+    }
   });
 });
 
@@ -35,7 +38,43 @@ function earth() {
   container.style.border = '6px groove green';
 }
 
+function escMeme1() {
+  let img = document.querySelector('img');
+  img.onload = () => {
+    URL.revokeObjectURL(img.src);
+  };
+  img.src = 'imgs/meme1.png'
+}
+
+function escMeme2() {
+  let img = document.querySelector('img');
+  img.onload = () => {
+    URL.revokeObjectURL(img.src);
+  };
+  img.src = 'imgs/meme2.png'
+}
+
+function escMeme3() {
+  let img = document.querySelector('img');
+  img.onload = () => {
+    URL.revokeObjectURL(img.src);
+  };
+  img.src = 'imgs/meme3.png'
+}
+
+function escMeme4() {
+  let img = document.querySelector('img');
+  img.onload = () => {
+    URL.revokeObjectURL(img.src);
+  };
+  img.src = 'imgs/meme4.png'
+}
+
 memeInput.addEventListener('keyup', textoMeme);
 fireButton.addEventListener('click', fire);
 waterButton.addEventListener('click', water);
 earthButton.addEventListener('click', earth);
+imgMeme1.addEventListener('click', escMeme1);
+imgMeme2.addEventListener('click', escMeme2);
+imgMeme3.addEventListener('click', escMeme3);
+imgMeme4.addEventListener('click', escMeme4);
