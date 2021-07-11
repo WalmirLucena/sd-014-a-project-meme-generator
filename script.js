7548b80cd267218d9783c9e10ad34f5db47d5562
Ilan Aragão <ilan_aragao@hotmail.com>
2021-07-11 19:48:53 -0300

    Refatorando o JavaScript

--- script.js
+++ script.js
@@ -1,44 +1,77 @@
+// Graças ao PR do Kristiano Kasper (https://github.com/tryber/sd-014-a-project-meme-generator/pull/5), dei uma refatorada no meu javascript para passar no eslint //
+
 const textInput = document.querySelector('#text-input');
 const memeText = document.querySelector('#meme-text');
-textInput.addEventListener('input', function () {
-  memeText.innerText = textInput.value;
-});
-
-const memeInsert = document.querySelector('#meme-insert');
-const memeImage = document.querySelector('#meme-image');
-memeInsert.addEventListener('change', function (event) {
-  memeImage.src = URL.createObjectURL(event.target.files[0]);
-});
-
-const addBorda = document.querySelector('#meme-image-container');
+const imageInput = document.querySelector('#meme-insert');
+const memeImg = document.querySelector('#meme-image');
+const container = document.querySelector('#meme-image-container');
 const fireButton = document.querySelector('#fire');
-fireButton.addEventListener('click', function () {
-  addBorda.style.border = '3px dashed red';
-});
-
 const waterButton = document.querySelector('#water');
-waterButton.addEventListener('click', function () {
-  addBorda.style.border = '5px double blue';
-});
-
 const earthButton = document.querySelector('#earth');
-earthButton.addEventListener('click', function () {
-  addBorda.style.border = '6px groove green';
-});
+const img1 = document.querySelector('#meme-1');
+const img2 = document.querySelector('#meme-2');
+const img3 = document.querySelector('#meme-3');
+const img4 = document.querySelector('#meme-4');
 
-const meme1 = document.querySelector('#meme-1');
-meme1.addEventListener('click', function () {
-  memeImage.src = meme1.src;
-});
-const meme2 = document.querySelector('#meme-2');
-meme2.addEventListener('click', function () {
-  memeImage.src = meme2.src;
-});
-const meme3 = document.querySelector('#meme-3');
-meme3.addEventListener('click', function () {
-  memeImage.src = meme3.src;
-});
-const meme4 = document.querySelector('#meme-4');
-meme4.addEventListener('click', function () {
-  memeImage.src = meme4.src;
-});
+function addTexto() {
+  textInput.addEventListener('input', () => {
+    memeText.innerText = textInput.value;
+  });
+}
+addTexto();
+
+function addImagem() {
+  imageInput.addEventListener('change', () => {
+    memeImg.src = URL.createObjectURL(imageInput.files[0]);
+  });
+}
+addImagem();
+
+function fire() {
+  fireButton.addEventListener('click', () => {
+    container.style.border = '3px dashed red';
+  });
+}
+fire();
+
+function water() {
+  waterButton.addEventListener('click', () => {
+    container.style.border = '5px double blue';
+  });
+}
+water();
+
+function earth() {
+  earthButton.addEventListener('click', () => {
+    container.style.border = '6px groove green';
+  });
+}
+earth();
+
+function meme1() {
+  img1.addEventListener('click', () => {
+    memeImg.src = 'imgs/meme1.png';
+  });
+}
+meme1();
+
+function meme2() {
+  img2.addEventListener('click', () => {
+    memeImg.src = 'imgs/meme2.png';
+  });
+}
+meme2();
+
+function meme3() {
+  img3.addEventListener('click', () => {
+    memeImg.src = 'imgs/meme3.png';
+  });
+}
+meme3();
+
+function meme4() {
+  img4.addEventListener('click', () => {
+    memeImg.src = 'imgs/meme4.png';
+  });
+}
+meme4();
