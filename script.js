45dd5ad3df6e18f4db0a0c6ecd1e7708abb93717
Jonathan Ferreira <j0n4t@hotmail.com>
2021-07-11 20:27:36 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const inputText = document.getElementById('text-input');
+const inputImage = document.getElementById('meme-insert');
 const memeText = document.getElementById('meme-text');
-const imageContainer = document.getElementById('meme-image-container');
+const memeImage = document.getElementById('meme-image');
 
 function updateMemeText(event) {
   memeText.innerText = event.target.value;
 }
 
-inputText.addEventListener('keyup', updateMemeText)
+// Inspiração: https://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html
+function updateMemeImage(event) {
+  if (event.target.files && event.target.files[0]) {
+    const imageURL = URL.createObjectURL(event.target.files[0]);
+    memeImage.src = imageURL;
+  }
+}
+
+inputText.addEventListener('keyup', updateMemeText);
+inputImage.addEventListener('change', updateMemeImage);
