cf1fb60d23d207d499fbf2aa3676bcd75aa8a2a0
caiquequaresmasilva <caique.quaresma@gmail.com>
2021-07-11 18:53:20 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const textInput = document.getElementById('text-input');
+const memeContainer = document.getElementById('meme-image-container');
 const memeText = document.getElementById('meme-text');
 const memeInsert = document.getElementById('meme-insert');
 const memeImage = document.getElementById('meme-image');
+const buttonsContainer = document.getElementById('buttons-container');
 
 memeInsert.addEventListener('change', () => {
   // Fonte: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
@@ -11,4 +13,16 @@ memeInsert.addEventListener('change', () => {
 
 textInput.addEventListener('input', () => {
   memeText.innerText = textInput.value;
+});
+
+buttonsContainer.addEventListener('click', (event) => {
+  const button = event.target;
+  if (button.tagName === 'BUTTON' && !button.classList.contains('active')) {
+    const activeButton = buttonsContainer.querySelector('.active');
+    if (activeButton) {
+      activeButton.classList.toggle('active');
+    }
+    button.classList.toggle('active');
+    memeContainer.className = button.id;
+  }
 });
