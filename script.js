13da877881f20898e77cf0e025383217d2531da8
Williamns <williamnstolb@gmail.com>
2021-07-12 00:13:40 -0300

    100%

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const texto = document.querySelector('#meme-text');
 const input = document.querySelector('#text-input');
+const memeImage = document.querySelector('#meme-image');
 // Coloca texto na tela.
 input.addEventListener('input', () => {
   texto.innerText = input.value;
 });
 // Add imagem do meme:
-const memeImage = document.querySelector("#meme-image");
-let pathImage = '/imagens/eu.jpg';
 const buttonCarregaImagem = document.getElementById('meme-insert');
 buttonCarregaImagem.addEventListener('click', () => {
-  memeImage.src = pathImage;
+  memeImage.src = '/imgs/eu.jpg';
 });
 const borda = document.querySelector('#meme-image-container').style;
 // Borda vermelha:
@@ -26,4 +25,28 @@ buttonWater.addEventListener('click', () => {
 const buttonEarth = document.querySelector('#earth');
 buttonEarth.addEventListener('click', () => {
   borda.border = '6px groove green';
+});
+// Add meme da miniatura 1:
+const memeUm = document.querySelector('#meme-1');
+memeUm.addEventListener('click', () => {
+  console.log('entrou');
+  memeImage.src = '/imgs/meme1.png';
+});
+// Add meme da miniatura 2:
+const memeDois = document.querySelector('#meme-2');
+memeDois.addEventListener('click', () => {
+  console.log('entrou');
+  memeImage.src = '/imgs/meme2.png';
+});
+// Add meme da miniatura 1:
+const memeTres = document.querySelector('#meme-3');
+memeTres.addEventListener('click', () => {
+  console.log('entrou');
+  memeImage.src = '/imgs/meme3.png';
+});
+// Add meme da miniatura 1:
+const memeQuatro = document.querySelector('#meme-4');
+memeQuatro.addEventListener('click', () => {
+  console.log('entrou');
+  memeImage.src = '/imgs/meme4.png';
 });
