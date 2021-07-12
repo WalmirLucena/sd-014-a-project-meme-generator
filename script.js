d4ef02d2fed720fc87701f16e4057ad302e03439
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-07-12 19:40:36 -0300

    requisito 7 e arruma o lint

--- script.js
+++ script.js
@@ -17,15 +17,35 @@ const btnWater = document.getElementById('water');
 const btnEarth = document.getElementById('earth');
 const containerMeme = document.getElementById('meme-image-container');
 
-
 btnFire.addEventListener('click', () => {
-  containerMeme.className = 'fire'
+  containerMeme.className = 'fire';
 });
 
 btnWater.addEventListener('click', () => {
-  containerMeme.className = 'water'
+  containerMeme.className = 'water';
 });
 
 btnEarth.addEventListener('click', () => {
-  containerMeme.className = 'earth'
+  containerMeme.className = 'earth';
+});
+
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+const meme4 = document.getElementById('meme-4');
+
+meme1.addEventListener('click', () => {
+  memeImage.src = 'imgs/meme1.png';
+});
+
+meme2.addEventListener('click', () => {
+  memeImage.src = 'imgs/meme2.png';
+});
+
+meme3.addEventListener('click', () => {
+  memeImage.src = 'imgs/meme3.png';
+});
+
+meme4.addEventListener('click', () => {
+  memeImage.src = 'imgs/meme4.png';
 });
