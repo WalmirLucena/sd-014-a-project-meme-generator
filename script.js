9b31a2b4833224481838cbbcb8f27be9403192d1
Gustavo Dolzan <gudolzan@gmail.com>
2021-07-12 18:34:27 -0300

    Adds font

--- script.js
+++ script.js
@@ -2,11 +2,9 @@ const textInput = document.getElementById('text-input');
 const memeInsert = document.getElementById('meme-insert');
 const memeText = document.getElementById('meme-text');
 
-
 textInput.addEventListener('keyup', () => { // Loads text
   memeText.innerText = textInput.value;
 });
-
 
 memeInsert.addEventListener('change', (event) => { // Loads image
   const image = document.getElementById('meme-image');
