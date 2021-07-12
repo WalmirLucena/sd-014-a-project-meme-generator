4b3910148b46e854f5941aa5fdaed452635958ec
Gabriel <gabrieleliasbenedetti@hotmail.com>
2021-07-12 19:08:45 -0300

    requisito 6

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ const chooseFile = document.getElementById('meme-insert');
 chooseFile.addEventListener('change', (event) => {
   memeImage.src = URL.createObjectURL(event.target.files[0]);
 });
+
+const btnFire = document.getElementById('fire');
+const btnWater = document.getElementById('water');
+const btnEarth = document.getElementById('earth');
+const containerMeme = document.getElementById('meme-image-container');
+
+
+btnFire.addEventListener('click', () => {
+  containerMeme.className = 'fire'
+});
+
+btnWater.addEventListener('click', () => {
+  containerMeme.className = 'water'
+});
+
+btnEarth.addEventListener('click', () => {
+  containerMeme.className = 'earth'
+});
