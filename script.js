9e07e99c30190994609545182dadcf55d87b95b3
WilsonSalles-Jnr <wilsonsallesjunior@gmail.com>
2021-07-12 12:57:09 -0300

    faltando apenas duas para concluir

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const textIpt = document.getElementById('text-input');
 const par = document.getElementById('meme-text');
 const file = document.getElementById('meme-insert');
 const img = document.getElementsByTagName('img')[0];
+const fire = document.getElementById('fire')
+const water = document.getElementById('water')
+const earth = document.getElementById('earth')
+const cont = document.getElementById('meme-image-container')
 
 function generate(enter){
     if(enter.key !== 'F11'){
@@ -18,4 +22,16 @@ function selectImage(event){
     img.onload = function(){
         URL.revokeObjectURL(img.src)
     } // referência: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded
+}
+
+fire.addEventListener('click', Fire)
+water.addEventListener('click', Water)
+earth.addEventListener('click', Earth)
+
+function Fire(){
+    cont.className = 'fire'
+}function Water(){
+    cont.className = 'water'
+}function Earth(){
+    cont.className = 'earth'
 }
