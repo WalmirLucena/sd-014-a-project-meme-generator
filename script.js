632888f2690582e4c61c08bd5130ed10556c3045
Gustavo Dolzan <gudolzan@gmail.com>
2021-07-12 17:50:48 -0300

    Adds load text function

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function loadImage(event) {
+const textInput = document.getElementById('text-input');
+const memeInsert = document.getElementById('meme-insert');
+const memeText = document.getElementById('meme-text');
+
+textInput.addEventListener('keyup', () => { // Loads text
+  memeText.innerText = textInput.value;
+});
+
+memeInsert.addEventListener('change', (event) => { // Loads image
   const image = document.getElementById('meme-image');
   image.src = URL.createObjectURL(event.target.files[0]);
-}
-loadImage();
+});
