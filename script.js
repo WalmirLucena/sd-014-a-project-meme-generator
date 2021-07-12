8d34f087758980787572da11f0ff670af948b76c
Ivan Zigoni <ivanzigonipires.11@gmail.com>
2021-07-12 18:59:49 -0300

    Lint

--- script.js
+++ script.js
@@ -10,20 +10,20 @@ const btnFire = document.getElementById('fire');
 function memeText() {
   textInputContainer.innerText = input.value;
   if (input.value.length === 0) {
-    input.value = "";
+    input.value = '';
   }
 }
 
 function addImg() {
   const reader = new FileReader();
-  const file = imgInput.files[0]
+  const file = imgInput.files[0];
   if (file) {
-	  reader.readAsDataURL(file);
-	}
-  reader.addEventListener('load', teste) 
+    reader.readAsDataURL(file);
+  }
   function teste() {
-	  imgContainer.src = reader.result;
-	}
+    imgContainer.src = reader.result;
+  }
+  reader.addEventListener('load', teste);
 }
 
 function greenBorder() {
@@ -38,12 +38,18 @@ function redBorder() {
 
 function blueBorder() {
   container.style.border = '';
-  container.style.border = '5px double blue'
+  container.style.border = '5px double blue';
 }
 
+function addPreview(e) {
+  imgContainer.src = e.target.src;
+}
 
 imgInput.addEventListener('change', addImg);
 input.addEventListener('keyup', memeText);
 btnEarth.addEventListener('click', greenBorder);
 btnFire.addEventListener('click', redBorder);
 btnWater.addEventListener('click', blueBorder);
+for (let i = 0; i < document.getElementById('preview-container').children.length; i += 1) {
+  document.getElementById('preview-container').children[i].addEventListener('click', addPreview);
+}
