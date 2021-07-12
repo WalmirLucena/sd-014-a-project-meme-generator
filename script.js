85f68c07d7868d2638b99ca6a23524cd854805fd
Gustavo Dolzan <gudolzan@gmail.com>
2021-07-12 19:16:59 -0300

    Adds frame buttons

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const textInput = document.getElementById('text-input');
 const memeInsert = document.getElementById('meme-insert');
 const memeText = document.getElementById('meme-text');
+const memeImageContainer = document.getElementById('meme-image-container');
 
 textInput.addEventListener('keyup', () => { // Loads text
   memeText.innerText = textInput.value;
@@ -9,4 +10,11 @@ textInput.addEventListener('keyup', () => { // Loads text
 memeInsert.addEventListener('change', (event) => { // Loads image
   const image = document.getElementById('meme-image');
   image.src = URL.createObjectURL(event.target.files[0]);
+});
+
+document.querySelectorAll('button').forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const borderStyle = window.getComputedStyle(event.target, null).getPropertyValue('border');
+    memeImageContainer.style.border = borderStyle;
+  });
 });
