25edab4c0a4b5377e43de10d33fc6470ebfc6e8b
Douglas Souza <douglassouzabjj@hotmail.com>
2021-07-12 16:32:21 -0300

    Adiciona requisitos bonus

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const fileInput = document.getElementById('meme-insert');
 const container = document.getElementById('meme-image-container');
 const memeText = document.getElementById('meme-text');
 const image = document.getElementById('meme-image');
+const btnFire = document.getElementById('fire');
+const btnWater = document.getElementById('water');
+const btnEarth = document.getElementById('earth');
+const suggestion1 = document.getElementById('meme-1');
+const suggestion2 = document.getElementById('meme-2');
+const suggestion3 = document.getElementById('meme-3');
+const suggestion4 = document.getElementById('meme-4');
 
 // Adiciona texto digitado a div dentro da imagem
 function addText() {
@@ -21,5 +28,43 @@ function addImage(event) {
   fr.readAsDataURL(selectedFile);
 }
 
+// Adiciona estilo da classe fire ao container
+function addFireStyle() {
+  container.style.border = '3px dashed red'
+}
+
+// Adiciona estilo da classe water ao container
+function addWaterStyle() {
+  container.style.border = '5px double blue'
+}
+
+// Adiciona estilo da classe earth ao container
+function addEarthStyle() {
+  container.style.border = '6px groove green'
+}
+
+function addFirstSuggestion() {
+  image.src = suggestion1.src;
+}
+
+function addSecondSuggestion() {
+  image.src = suggestion2.src;
+}
+
+function addThirdSuggestion() {
+  image.src = suggestion3.src;
+}
+
+function addFourthSuggestion() {
+  image.src = suggestion4.src;
+}
+
 textInput.addEventListener('input', addText);
 fileInput.addEventListener('change', addImage);
+btnFire.addEventListener('click', addFireStyle);
+btnWater.addEventListener('click', addWaterStyle);
+btnEarth.addEventListener('click', addEarthStyle);
+suggestion1.addEventListener('click', addFirstSuggestion);
+suggestion2.addEventListener('click', addSecondSuggestion);
+suggestion3.addEventListener('click', addThirdSuggestion);
+suggestion4.addEventListener('click', addFourthSuggestion);
