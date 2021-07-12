37db478faff4c0b7a86a3259cdf1151dce8892ab
Williamns <williamnstolb@gmail.com>
2021-07-11 23:21:33 -0300

    Completo: item 6

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 const texto = document.querySelector('#meme-text');
-console.log(texto);
 const input = document.querySelector('#text-input');
-console.log(input);
 // Coloca texto na tela.
 input.addEventListener('input', () => {
   texto.innerText = input.value;
 });
-
-function carregaImagem() {
-  const memeImage = document.querySelector("#meme-image");
-  memeImage.src = '/imagens/eu.jpg';
-}
+// Add imagem do meme:
+const memeImage = document.querySelector("#meme-image");
+let pathImage = '/imagens/eu.jpg';
 const buttonCarregaImagem = document.getElementById('meme-insert');
-buttonCarregaImagem.addEventListener('change', carregaImagem);
+buttonCarregaImagem.addEventListener('click', () => {
+  memeImage.src = pathImage;
+});
+const borda = document.querySelector('#meme-image-container').style;
+// Borda vermelha:
+const buttonFire = document.querySelector('#fire');
+buttonFire.addEventListener('click', () => {
+  borda.border = '3px dashed red';
+});
+// Borda azul:
+const buttonWater = document.querySelector('#water');
+buttonWater.addEventListener('click', () => {
+  borda.border = '5px double blue';
+});
+// Borda verde:
+const buttonEarth = document.querySelector('#earth');
+buttonEarth.addEventListener('click', () => {
+  borda.border = '6px groove green';
+});
