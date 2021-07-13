3152636d2df3a79928a5e0cc32d3b72f59a638cd
Pedro Silva Buente <pedrobuente@gmail.com>
2021-07-13 14:14:20 -0300

    requisitos

--- script.js
+++ script.js
@@ -37,4 +37,5 @@ createSelectorFile.addEventListener('change',function(imagem){
     document.getElementById('meme-image').src = URL.createObjectURL(imagem.target.files[0]);
     
 })
+
 // Referencias <https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/this | https://developer.mozilla.org/pt-BR/docs/Web/API/FileReader |https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977 | https://www.horadecodar.com.br/2020/05/20/javascript-preview-de-imagem-carregada-em-input-file/>
