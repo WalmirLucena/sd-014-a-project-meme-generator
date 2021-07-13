971aeafce2e1ad09c7a4b77fd2fcfe20a1981019
Dogl4 <doougllas@hotmail.com.br>
2021-07-13 01:53:25 -0300

    c2 - Adiciona upload de uma imagem do computador para o site.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const objEle = {
   textInput: document.querySelector('#text-input'),
   textMeme: document.querySelector('#meme-text'),
+  imagemUpload: document.querySelector('#meme-insert'),
+  imagemLoad: document.querySelector('#meme-image'),
 };
 
 const objFun = {
@@ -9,8 +11,14 @@ const objFun = {
       objEle.textMeme.innerText = objEle.textInput.value;
     });
   },
+  leia: function leImagem() {
+    objEle.imagemUpload.addEventListener('change', (event) => {
+      objEle.imagemLoad.src = URL.createObjectURL(event.target.files[0]);
+    });
+  }, // Referencias <https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/this | https://developer.mozilla.org/pt-BR/docs/Web/API/FileReader |https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977 | https://www.horadecodar.com.br/2020/05/20/javascript-preview-de-imagem-carregada-em-input-file/>
 };
 
 window.onload = () => {
   objFun.pegaTexto();
+  objFun.leia();
 };
