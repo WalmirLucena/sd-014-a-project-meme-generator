b4e3606d09815a62599aa2b6130043559b180197
Dogl4 <doougllas@hotmail.com.br>
2021-07-13 03:06:04 -0300

    c6 - Adiciona customizar o meme escolhido acrescentando a ele uma de três bordas a sua escolha.

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const objEle = {
   textMeme: document.querySelector('#meme-text'),
   imagemUpload: document.querySelector('#meme-insert'),
   imagemLoad: document.querySelector('#meme-image'),
+  fire: document.querySelector('#fire'),
+  water: document.querySelector('#water'),
+  earth: document.querySelector('#earth'),
+  bordaImagem: document.querySelector('#meme-image-container'),
 };
 
 const objFun = {
@@ -17,6 +21,17 @@ const objFun = {
     });
   }, // Referencias <https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Operators/this | https://developer.mozilla.org/pt-BR/docs/Web/API/FileReader |https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977 | https://www.horadecodar.com.br/2020/05/20/javascript-preview-de-imagem-carregada-em-input-file/>
 };
+
+objEle.fire.addEventListener('click', () => {
+  
+  objEle.bordaImagem.style.setProperty('border', '3px dashed red');
+});
+objEle.water.addEventListener('click', () => {
+  objEle.bordaImagem.style.setProperty('border', '5px double blue');
+});
+objEle.earth.addEventListener('click', () => {
+  objEle.bordaImagem.style.setProperty('border', '6px groove green');
+});
 
 window.onload = () => {
   objFun.pegaTexto();
