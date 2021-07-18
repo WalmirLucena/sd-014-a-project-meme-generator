86bffc94278b06849998a91d71a3a9241cb37de0
WalmirLucena <walmirlucena3@gmail.com>
2021-07-18 12:11:09 -0400

    Adiciona Input image e Requisito 2

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+
 const memeText = document.getElementById("meme-text");
 const getInput = document.getElementById("text-input");
+const getImageContainer = document.getElementById('meme-image');
 
+//Requisito 1 
 getInput.addEventListener('keyup', function (event) {
   memeText.innerText = getInput.value;
 })
+
+//Requisito 2
+// URL.createObjectURL() - cria uma url a partir de um arquivo
+function uploadImage(){
+  const getImage = document.getElementById("meme-insert");
+  getImage.addEventListener("change", function(event){
+  
+    getImageContainer.src = URL.createObjectURL(event.target.files[0]);
+  })
+}
+uploadImage();
