50751022f2a7abd5f706abd5db95a82993dae443
WalmirLucena <walmirlucena3@gmail.com>
2021-07-18 13:42:03 -0400

    Adiciona as Bordas

--- script.js
+++ script.js
@@ -1,45 +1,35 @@
-
 const memeText = document.getElementById("meme-text");
 const getInput = document.getElementById("text-input");
 const getImageContainer = document.getElementById('meme-image');
-const memeContainer=document.getElementById("meme-image-container")
+const memeContainer = document.getElementById("meme-image-container")
 
 //Requisito 1 
 getInput.addEventListener('keyup', function (event) {
-  memeText.innerText = getInput.value;
+  memeText.innerHTML = getInput.value;
 })
 
 //Requisito 2
 // URL.createObjectURL() - cria uma url a partir de um arquivo
-function uploadImage(){
+function uploadImage() {
   const getImage = document.getElementById("meme-insert");
-  getImage.addEventListener("change", function(event){
-  
+  getImage.addEventListener("change", function (event) {
+
     getImageContainer.src = URL.createObjectURL(event.target.files[0]);
   })
 }
 uploadImage();
 
-function buttonFire(){
-  const getFire = document.getElementById('fire');
-  getFire.addEventListener("click", function(event){
+const getButtons = document.getElementById("buttons");
+
+getButtons.addEventListener('click', function name(event){
+  const buttonId = event.target.id;
+  if(buttonId === 'fire'){
     memeContainer.style.border = '3px dashed red';
-  })
-}
-buttonFire();
-
-function buttonWater(){
-  const getFire = document.getElementById('water');
-  getFire.addEventListener("click", function(event){
+  } else if( buttonId === 'water'){
     memeContainer.style.border = '5px double blue';
-  })
-}
-buttonWater();
-
-function buttonEarth(){
-  const getFire = document.getElementById('earth');
-  getFire.addEventListener("click", function(event){
+  } else if(buttonId === 'earth'){
     memeContainer.style.border = '6px groove green';
-  })
-}
-buttonEarth();
+  }
+
+  
+})
