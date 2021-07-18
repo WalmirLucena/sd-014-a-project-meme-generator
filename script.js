b946b1ad6c5116300ffad93547f79ba078fca5f5
Filipe Brochier <filipe.brochier@gmail.com>
2021-07-18 17:55:39 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const obj = {
   textInput: document.getElementById('text-input'),
   memeText: document.getElementById('meme-text'),
+  memeInsert: document.getElementById('meme-insert'),
+  memeImageContainer: document.getElementById('meme-image-container'),
+  memeImage: document.getElementById('meme-image'),
 };
 
 obj.textInput.addEventListener('keyup', () => {
-  const textInput = document.getElementById('text-input').value;
-  obj.memeText.innerText = textInput;
+  const textInputValue = document.getElementById('text-input').value;
+  obj.memeText.innerText = textInputValue;
 });
+
+obj.memeInsert.addEventListener('change', () => {
+  const image = document.getElementById('meme-image');
+  const memeInsertValue = document.getElementById('meme-insert').files[0];
+  image.src = window.URL.createObjectURL(memeInsertValue);
+  obj.memeImageContainer.appendChild(image);
+});
+
+// Método createObjectURL: Pode ser encontrado em: https://stackoverflow.com/a/27165977
